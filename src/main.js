b231ad99a5831bb72b2300bd938e827663dfcd06
Wade Pate <wadejp8@gmail.com>
2021-11-02 09:07:55 -0600

    squashed commits into one
    removed HelloWorld.vue
    
    added comments in main.js to example global components usage for future ref
    
    edited .eslint.js as per instructed by pluralsight guide
    
    created HomePage.vue for home page local component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+// import HomePage from './components/HomePage.vue'; // * need to import component to make it global
 
-createApp(App).mount('#app');
+createApp(App).mount('#app'); // * returns newly created vue app
+
+//  * example of registering a component a global component
+//  * Global variables should be avoided as it increases bundle size
+//  * Only use global IF truly global and needed to avoid naming issues and more...
+// const app = createApp(App).mount("#app");
+// app.component('HomePage', HomePage);
